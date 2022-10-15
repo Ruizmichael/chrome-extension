f6c83842f0e907e5766d45a3a3d530de3e440137
mashkn <mariya.novokhatska@gmail.com>
2022-10-15 10:10:22 -0600

    update

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 // Select all tags on html
 const contentArray = document.querySelectorAll('*');
 // loop through contentArray and style backgroundColor
-
 const startDarkMode = (array) => {
     array.forEach(element => {
-        element.style.backgroundColor = "rgb(45, 45, 45)";
+        element.style.backgroundColor = "black";
         element.style.color = 'rgb(201, 201, 201)';
 
     });
 };
 
-const colorOptions = [];
-
-const checkbox = document.querySelector("input[name=checkbox]");
+const checkbox = chrome.extension.getElementById("myCheck");
+console.log(checkbox);
 checkbox.addEventListener('change', (event) => {
-    const { checked } = checkbox;
+    const checked  = checkbox;
     toggleContent(checked);
     startDarkMode(contentArray);
     });
 
 
-//declare a state variable 
+// declare a state variable 
 // let state = false;
 // chrome.browserAction.onClicked.addListener(function(tab) {
 //     if(!state){
 //         // startDarkMode(contentArray);
-//         chrome.tabs.insertCSS(null, { file: "styles.css"})
+//         startDarkMode(contentArray);
 //         state = !state;
 //         return;
 //     }
