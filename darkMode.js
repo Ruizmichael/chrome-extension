cbc710a886ac15d61542cbc3eb0c3495242236a4
Michael Ruiz <maruiz.sc1@gmail.com>
2022-10-15 11:57:54 -0700

    Co-authored-by: mashkn <mashkn@users.noreply.github.com>

---
+++ darkMode.js
@@ -0,0 +1,11 @@
+(function () {
+    // const contentArray = document.querySelector('*')
+    document.querySelector("*").style.filter = "invert(1) hue-rotate(180deg)";
+
+    console.log('darkmode on');
+    // contentArray.forEach(element => {
+    //     element.style.backgroundColor = "black";
+    //     element.style.color = 'rgb(201, 201, 201)';
+
+    // });
+})()
--- main.js
+++ main.js
@@ -1,21 +1,50 @@
+if (document.querySelector(".popup")) {
 // Select all tags on html
-const contentArray = document.querySelectorAll('*');
+    const contentArray = document.querySelectorAll('body');
+    const button = document.querySelector('.switch');
+    let buttonOn = false;
+
+
 // loop through contentArray and style backgroundColor
-const startDarkMode = (array) => {
-    array.forEach(element => {
-        element.style.backgroundColor = "black";
-        element.style.color = 'rgb(201, 201, 201)';
+// const startDarkMode = (array) => {
+//     array.forEach(element => {
+//         element.style.backgroundColor = "black";
+//         element.style.color = 'rgb(201, 201, 201)';
 
-    });
-};
+//     });
+// };
 
-const checkbox = chrome.extension.getElementById("myCheck");
-console.log(checkbox);
-checkbox.addEventListener('change', (event) => {
-    const checked  = checkbox;
-    toggleContent(checked);
-    startDarkMode(contentArray);
-    });
+// const startLightMode = (array) => {
+//     array.forEach(element => {
+//         element.style.backgroundColor = 'rgb(201, 201, 201)';
+//         element.style.color = 'black';
+
+//     });
+// };
+
+button.addEventListener('click', () => {
+    if (!buttonOn) {
+        buttonOn = true;
+        console.log('darkmode on')
+        chrome.tabs.executeScript({
+            file: 'darkMode.js'
+        })
+    } else {
+        button = false;
+        console.log('lightmode on')
+        chrome.tabs.executeScript({
+            file: 'lightMode.js'
+        })
+    }
+})
+}
+// const checkbox = chrome.runtime.onInstalled.getElementById("myCheck");
+// console.log(checkbox);
+// checkbox.addEventListener('change', (event) => {
+//     const checked  = checkbox;
+//     toggleContent(checked);
+//     startDarkMode(contentArray);
+//     });
 
 
 // declare a state variable 
